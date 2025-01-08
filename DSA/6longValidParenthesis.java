496cd10df5ecd2590ff58b0cab38a102bea987b5
Krishna-Davada <krishnadavada6@gmail.com>
2025-01-08 10:58:33 +0530

    new changes

--- DSA/6longValidParenthesis.java
+++ DSA/6longValidParenthesis.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution6 {
     public int longestValidParentheses(String s) {
         //initialize left and right counter as 0
         int l=0;
@@ -45,47 +45,13 @@ class Solution {
         }
         return max;
     }
-}
-
-class Solution5 {
-    public int getSecondLargest(int[] arr) {
-        
-        if(arr.length<2){
-            return -1;
-        }
-
-        int max1 = Integer.MIN_VALUE;  // The largest element
-        int max2 = Integer.MIN_VALUE;  // The second largest element
-        
-        // Initialize the largest and second largest elements
-        if(arr[0]>arr[1]){
-            max1=arr[0];
-            max2=arr[1];
-        }
-        else{
-            max1=arr[1];
-            max2=arr[0];
-        }
-        
-        for(int i=2;i<arr.length;i++){
-            if(arr[i]>max1){
-                max2=max1;
-                max1=arr[i];
-            }
-            else if(arr[i]>max2){
-                max2=arr[i];
-            }
-        }
-        return max2;
-    }
 
     public static void main(String[] args) {
-        Solution5 solution = new Solution5(); // Create an instance of the Solution class
-        int[] nums = {23,67,32,1,5,90,33,87};
-        System.out.println(solution.getSecondLargest(nums)); // Call the method
+        Solution6 solution = new Solution6(); // Create an instance of the Solution class
+        System.out.println(solution.longestValidParentheses("(()")); // Call the method
     }
-
 }
+
 
  //time complexity : O(n)
  //space complexity : O(1)
